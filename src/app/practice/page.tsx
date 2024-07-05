89a6430fca6d56a0683fa95849835f0a6a9d4517
João Pedro A <jaaopbr@gmail.com>
2024-07-05 02:18:07 -0300

    feat: add practice page

---
+++ src/app/practice/page.tsx
@@ -0,0 +1,9 @@
+import {Card, CardContent, CardHeader} from '@/components/ui/card';
+
+export default function Page() {
+  return (
+    <div className='flex flex-col lg:flex-row gap-6 p-8 h-full'>
+      <h1>Practice</h1>
+    </div>
+  )
+}
